86a4af73255866b5ccd7fd00f2b6bd555c13cd57
Barrigan <dcbarrigan@gmail.com>
2020-09-28 17:16:35 +0100

    Updates

---
+++ precache-manifest.4a600779b929474bed9d1ff7c38c5307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254f3808ccb96307abd1bacf7312825a",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "28f054f0d3b2b7629bd7",
+    "url": "/javascript-calculator/static/css/main.2051f4d1.chunk.css"
+  },
+  {
+    "revision": "b9924085a6edd708a94b",
+    "url": "/javascript-calculator/static/js/2.2209fa6e.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/javascript-calculator/static/js/2.2209fa6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f054f0d3b2b7629bd7",
+    "url": "/javascript-calculator/static/js/main.75ebd8cf.chunk.js"
+  },
+  {
+    "revision": "2a1d30e85db36d30dcbf",
+    "url": "/javascript-calculator/static/js/runtime-main.f3cb5538.js"
+  }
+]);
